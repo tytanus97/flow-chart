3148fcbbeeb04de62c12e0feea13952b510b66eb
pawel.ataman <pawel.ataman@finanteq.com>
2023-03-04 10:22:27 +0100

    create tester app

--- apps/flow-chart/src/app/app.component.ts
+++ apps/flow-chart/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'flow-chart-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true
 })
 export class AppComponent {
   title = 'flow-chart';
